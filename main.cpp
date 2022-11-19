262ca57983774f29cc602d20d2fc2ba1891f9625
fadelsaad99 <118170848+fadelsaad99@users.noreply.github.com>
2022-11-19 20:56:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
-
+#include <math.h>
 int main() {
-   return 0;
+    int opzione;
+    float a, b;
+    cout<<"metti i valori di a e b"<<endl;
+    cin>>a;
+    cin>>b;
+    switch (opzione) {
+        case 0:
+            cout<< ( a * b ) / 2 <<endl;
+            break;
+        case 1:
+            cout<< pow(a, 2)<<endl;
+            break;
+        case 2:
+            cout<< (a * b)<<endl;
+            break;
+        default:
+            cout<< " opzione non valida "<<endl;
+    }
+    return 0;
 }
